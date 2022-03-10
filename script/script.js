1bab48adb87ade1351e3e639becb196bc8c72684
AlexForget <alexandreqc26@gmail.com>
2022-03-10 08:25:38 -0500

    création strucutre projet et test style et script sont liés

---
+++ script/script.js
@@ -0,0 +1,7 @@
+var boutonTest = document.querySelector("#boutonTest");
+
+const couleur = () => {
+  document.body.style.backgroundColor = "red";
+};
+
+boutonTest.addEventListener("click", couleur);
