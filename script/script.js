204d4b394d95c74ff548c1cfc17f7480dd120624
AlexForget <alexandreqc26@gmail.com>
2022-03-10 19:51:41 -0500

    Ajout des premières fonctions de bases

--- script/script.js
+++ script/script.js
@@ -7,3 +7,35 @@ const couleur = () => {
 
 boutonTest.addEventListener("click", couleur);
 */
+
+const additioner = (nb1, nb2) => {
+  return nb1 + nb2;
+};
+
+const soustraitre = (nb1, nb2) => {
+  return nb1 - nb2;
+};
+
+const multiplier = (nb1, nb2) => {
+  return nb1 * nb2;
+};
+
+const diviser = (nb1, nb2) => {
+  if (nb2 === 0) {
+    return "Division par zéro impossible";
+  }
+  return nb1 / nb2;
+};
+
+const operation = (operateur, nb1, nb2) => {
+  switch (operateur) {
+    case "+":
+      return additioner(nb1, nb2);
+    case "-":
+      return soustraitre(nb1, nb2);
+    case "*":
+      return multiplier(nb1, nb2);
+    case "/":
+      return diviser(nb1, nb2);
+  }
+};
